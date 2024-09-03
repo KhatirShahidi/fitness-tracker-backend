e0fb1f021a391f1ed95f0ade0066e2ffa97af123
KhatirShahidi <khatirshahidi02@gmail.com>
2024-09-03 23:10:41 +0800

    Added ssl

--- database/connection.js
+++ database/connection.js
@@ -13,6 +13,7 @@ const database = new Pool({
     database: process.env.DB_DATABASE,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
+    ssl: process.env.DB_SSL === "true",
 });
 
 async function initializeDatabase() {
